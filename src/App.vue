aecffe6392a200ce28adff9b6156f2fe501fb939
abdullahhussein1 <abdullabale11@gmail.com>
2024-12-15 10:28:35 +0300

    Adding Basic Form with Validation using vee and yup

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,72 @@
 <script setup lang="ts">
-import { Form, Field } from 'vee-validate'
+import { useForm } from 'vee-validate'
+import { ref } from 'vue'
+import * as yup from 'yup'
 
-const onSubmit = (values) => {
-  console.log(values)
-}
+const limit = ref(5)
+
+const schema = yup.lazy(() =>
+  yup.object({
+    email: yup.string().required().email(),
+    password: yup.string().required('Password is Required').min(limit.value),
+  }),
+)
+
+const { errors, defineField, meta } = useForm({
+  validationSchema: schema,
+})
+
+const [email, emailAttrs] = defineField('email')
+const [password, passwordAttrs] = defineField('password')
 </script>
 
 <template>
   <div
     id="app"
-    class="bg-slate-950 min-h-screen flex flex-col text-emerald-500 items-center justify-center"
+    class="bg-slate-950 min-h-screen flex flex-col text-white items-center justify-center"
   >
-    <Form @submit="onSubmit">
-      <Field type="email" name="email" />
-      <button>Sign up for newsletter</button>
-    </Form>
+    <h1 class="text-4xl pb-12 text-emerald-500 font-extrabold tracking-tight">
+      Vee Validate <span class="text-xs text-white font-medium">with yup</span>
+    </h1>
+
+    <div class="flex flex-col gap-6 w-full max-w-md mx-auto items-center justify-center">
+      <div class="flex flex-col gap-1 w-full">
+        <label for="email" class="font-semibold"
+          >Email <span class="text-red-500 text-xs">*</span></label
+        >
+        <input
+          v-model="email"
+          v-bind="emailAttrs"
+          class="bg-slate-900/40 text-white/90 border-[1.5px] ring-0 outline-0 focus:border-emerald-500 hover:bg-slate-900/60 border-slate-800 py-1 px-2 rounded-lg text-sm"
+        />
+        <p class="text-sm text-red-500">{{ errors.email }}</p>
+      </div>
+      <div class="flex flex-col gap-1 w-full">
+        <label for="password" class="font-semibold"
+          >Password <span class="text-red-500 text-xs">*</span></label
+        >
+        <div class="flex gap-2">
+          <input
+            v-model="password"
+            type="password"
+            v-bind="passwordAttrs"
+            class="bg-slate-900/40 text-white/90 flex-1 outline-0 focus:border-emerald-500 border-[1.5px] ring-0 outline-emerald-500 hover:bg-slate-900/60 border-slate-800 py-1 px-2 rounded-lg text-sm"
+          />
+          <input
+            type="number"
+            v-model.number="limit"
+            class="bg-slate-900/40 text-white/90 ring-0 outline-0 focus:border-emerald-500 border-[1.5px] outline-emerald-500 hover:bg-slate-900/60 border-slate-800 py-1 px-2 rounded-lg text-sm"
+          />
+        </div>
+        <p class="text-sm text-red-500">{{ errors.password }}</p>
+      </div>
+      <button
+        :disabled="!meta.dirty || !meta.valid"
+        class="w-full bg-emerald-500 rounded-full disabled:bg-emerald-800 disabled:text-white/50 p-1"
+      >
+        Submit
+      </button>
+    </div>
+    <div class="absolute size-72 blur-3xl top-44 bg-emerald-900 opacity-20 rounded-full" />
   </div>
 </template>
